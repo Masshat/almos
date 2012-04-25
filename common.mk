114d3936e7748f060a17fba8528d5e0d7f486b40
Martin Pieuchot <martin.pieuchot@lip6.fr>
2012-04-25 12:34:37 +0200

    Move the toolchain define in the common mk file

--- common.mk
+++ common.mk
@@ -13,3 +13,11 @@ endif
 ifeq ($(ARCH), i386)
 CPUCFLAGS = -march=i386
 endif
+
+CCPREFIX= $(CPU)-unknown-elf-
+
+CC=	$(CCPREFIX)cc
+LD=	$(CCPREFIX)ld
+NM=	$(CCPREFIX)nm
+STRIP=	$(CCPREFIX)strip
+OCPY=	$(CCPREFIX)objcopy
