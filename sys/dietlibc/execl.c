e28cc47d699e552113eddedb74dc731d7047bc25
Ghassan Almaless <ghassan.almaless@lip6.fr>
2012-10-30 16:19:38 +0100

    dietlibc: ensure that argv ends with NULL regardless of va_arg buggy behavior

--- sys/dietlibc/execl.c
+++ sys/dietlibc/execl.c
@@ -12,16 +12,17 @@ int execl(const char *path, const char *arg, ...){
   char **argv,*tmp;
   va_start(ap, arg);
   va_copy(bak,ap);
-  n=1;
+  n=2;
   while ((tmp=va_arg(ap,char *)))
     ++n;
   va_end (ap);
   if ((argv=(char **)alloca(n*sizeof(char*)))) {
     for (i=0; i<n; ++i)
-      argv[i]=va_arg(bak,char *);
+	    argv[i]=va_arg(bak,char *);
     va_end (bak);
 
-    argv[0] = (char*)path;
+    argv[0]   = (char*)path;
+    argv[n-1] = NULL;
     return execve(path,argv,environ);
   }
   va_end (bak);
