494a5714369f654935a41419a9814fef699c373c
Ghassan Almaless <ghassan.almaless@lip6.fr>
2013-10-31 12:19:20 +0100

    soclib: update the DMA driver to cope with virtual addresses

--- kernel/drivers/soclib/soclib_dma.c
+++ kernel/drivers/soclib/soclib_dma.c
@@ -47,11 +47,13 @@ struct dma_context_s
   struct wait_queue_s pending;
 };
 
-
-static void dma_start_request(struct device_s *dma, dev_request_t *rq)
+static void dma_start_request(struct device_s *dma, dev_request_t *ptr)
 {
 	volatile uint32_t *base = dma->base;
-  
+	dev_request_t *rq;
+
+	rq = (ptr->flags & DEV_RQ_NESTED) ? ptr->data : ptr;
+
 	base[DMA_SRC_REG]      = (unsigned int)rq->src;
 	base[DMA_DST_REG]      = (unsigned int)rq->dst;
 	base[DMA_IRQ_DISABLED] = 0;
@@ -63,6 +65,7 @@ static void dma_irq_handler(struct irq_action_s *action)
 	struct dma_context_s *ctx;
 	struct device_s *dma;
 	dev_request_t *rq;
+	dev_request_t *frag;
 	dev_request_t *new_rq;
 	error_t err;
 	volatile uint32_t *base;
@@ -85,6 +88,24 @@ static void dma_irq_handler(struct irq_action_s *action)
 	}
 
 	rq = list_first(&ctx->request_queue, dev_request_t, list);
+
+	if(rq->flags & DEV_RQ_NESTED)
+	{
+		frag = rq->data;
+
+		event_set_error(&frag->event, err);
+		event_set_senderId(&frag->event, dma);
+		event_send(&frag->event, &current_cpu->le_listner);
+
+		if((err == 0) && (frag->data != NULL))
+		{
+			rq->data = frag->data;
+			dma_start_request(dma,frag->data);
+			cpu_spinlock_unlock(&dma->lock.val);
+			return;
+		}
+	}
+
 	list_unlink(&rq->list);
   
 	if(!(list_empty(&ctx->request_queue)))
@@ -107,22 +128,167 @@ static void dma_irq_handler(struct irq_action_s *action)
  
 	event_set_error(&rq->event, err);
 	event_set_senderId(&rq->event, dma);
-	event_set_priority(&rq->event, E_BLK);
 	event_send(&rq->event, &current_cpu->le_listner);
 }
+
+static void dma_fraglist_destroy(dev_request_t *ptr)
+{
+	kmem_req_t req;
+	dev_request_t *frag;
+
+	req.type = KMEM_DMA_REQUEST;
+	frag     = ptr;
+
+	while(frag != NULL);
+	{
+		req.ptr = frag;
+		frag    = frag->data;
+
+		kmem_free(&req);
+	}
+}
+
+static EVENT_HANDLER(dma_async_frag_event)
+{
+	dev_request_t *frag;
+	kmem_req_t req;
+	error_t err;
+
+	frag = event_get_argument(event);
+	err  = event_get_error(event);
+
+	assert(frag != NULL && "corrupted frag");
+
+	if(err)
+	{
+		dma_fraglist_destroy(frag);
+		return 0;
+	}
+
+	req.type = KMEM_DMA_REQUEST;
+	req.ptr  = frag;
+
+	kmem_free(&req);
+	return 0;
+}
+
+static error_t dma_frag_init(dev_request_t *frag, uint_t src_start, uint_t dst_start, uint_t count)
+{
+	frag->src   = task_vaddr2paddr(current_task, (void*)src_start);
+	frag->dst   = task_vaddr2paddr(current_task, (void*)dst_start);
+	frag->count = count;
+	frag->flags = 0;
+
+	if((frag->src == NULL) || (frag->dst == NULL))
+		return EPERM;
+
+	event_set_priority(&frag->event, E_FUNC);
+	event_set_handler(&frag->event, &dma_async_frag_event);
+	event_set_argument(&frag->event, frag);
+	return 0;
+}
+
+static error_t dma_fraglist_build(dev_request_t *rq)
+{
+	kmem_req_t req;
+	dev_request_t *frag;
+	dev_request_t *parent_frag;
+	struct slist_entry *root;
+	struct slist_entry *iter;
+	uint_t src_start;
+	uint_t dst_start;
+	uint_t count;
+	error_t err;
+
+	if(rq->count == 0)
+		return EIO;
+
+	src_start = (uint_t)rq->src;
+	dst_start = (uint_t)rq->dst;
+
+	if((src_start + rq->count) <= ((src_start & ~PMM_PAGE_MASK) + PMM_PAGE_SIZE) &&
+	   (dst_start + rq->count) <= ((dst_start & ~PMM_PAGE_MASK) + PMM_PAGE_SIZE))
+	{
+		rq->src = task_vaddr2paddr(current_task, rq->src);
+		rq->dst = task_vaddr2paddr(current_task, rq->dst);
+		return 0;
+	}
+
+	if((src_start & PMM_PAGE_MASK) || (dst_start & PMM_PAGE_MASK))
+		return ENOSYS;
+
+	req.type    = KMEM_DMA_REQUEST;
+	req.size    = sizeof(*rq);
+	req.flags   = AF_KERNEL;
+	root        = (struct slist_entry *)&rq->data;
+	iter        = (struct slist_entry *)&rq->list;
+	count       = rq->count;
+	rq->data    = NULL;
+	rq->flags  &= ~DEV_RQ_NESTED;
+	parent_frag = rq;
+
+	while(count)
+	{
+		frag = kmem_alloc(&req);
+
+		if(frag == NULL)
+		{
+			err = ENOMEM;
+			goto fail_nomem;
+		}
+
+		if(count >= PMM_PAGE_SIZE)
+		{
+			err = dma_frag_init(frag, src_start, dst_start, PMM_PAGE_SIZE);
+
+			if(err)
+				goto fail_init;
+
+			src_start += PMM_PAGE_SIZE;
+			dst_start += PMM_PAGE_SIZE;
+			count     -= PMM_PAGE_SIZE;
+		}
+		else
+		{
+			err = dma_frag_init(frag, src_start, dst_start, count);
+
+			if(err)
+				goto fail_init;
+
+			count -= count;
+		}
+
+		parent_frag->data = frag;
+		frag->data        = NULL;
+		parent_frag       = frag;
+	}
+
+	rq->flags |= DEV_RQ_NESTED;
+	return 0;
+
+fail_init:
+fail_nomem:
+	dma_fraglist_destroy(rq->data);
+	return err;
+}
+
 
 static sint_t dma_request(struct device_s *dma, dev_request_t *rq)
 {
 	struct dma_context_s *ctx;
 	uint_t irq_state;
+	sint_t err;
 
-	ctx = (struct dma_context_s*)dma->data;
+	rq->err = 0;
+	ctx     = (struct dma_context_s*)dma->data;
 
-	//rq->src = task_vaddr2paddr(current_task, rq->src);
-	//rq->dst = task_vaddr2paddr(current_task, rq->dst);
+	err = dma_fraglist_build(rq);
+
+	if(err)
+		return err;
 
 	spinlock_lock_noirq(&dma->lock,&irq_state);
- 
+
 	if(list_empty(&ctx->request_queue))
 	{
 		list_add(&ctx->request_queue, &rq->list);
@@ -130,13 +296,13 @@ static sint_t dma_request(struct device_s *dma, dev_request_t *rq)
 	}
 	else
 		list_add_last(&ctx->request_queue, &rq->list);
- 
+
 	if(rq->flags & DEV_RQ_NOBLOCK)
 	{
 		spinlock_unlock_noirq(&dma->lock, irq_state);
 		return 0;
 	}
-   
+
 	wait_on(&ctx->pending, WAIT_LAST);
 	spinlock_unlock_noirq(&dma->lock,irq_state);
 	sched_sleep(current_thread);
@@ -156,7 +322,7 @@ error_t soclib_dma_init(struct device_s *dma, void *base, uint_t size, uint_t ir
 {
 	kmem_req_t req;
 	struct dma_context_s *ctx;
-  
+
 	spinlock_init(&dma->lock,"DevDMA (SoCLib)");
 	dma->base = base;
 	dma->irq  = irq;
